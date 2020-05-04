478fcb240da06d16058a3c35c9900927e3b69c6b
adrianpalomares <adrianpal45@gmail.com>
2020-05-03 23:39:38 -0700

    changed file structure

---
+++ server/api/routes.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+// Setting up router
+const router = express.Router();
+
+// Setting mongoose url
+const mongooseUrl = process.env.MONGODB_URL || "mongodb://localhost:27017/todoapp";
+
+mongoose.connect(mongooseUrl, { urlNewParser: true });
+
+//Todo Schema
+const Todo = mongoose.model("todos", {
+  title: String,
+  content: String
+});
+
+//GET
+router.get("/api/todos", function(request, response) {
+  Todo.find({}, function(err, docs) {
+    if (err) {
+      console.log(err);
+    } else {
+      response.json(docs);
+    }
+  });
+});
+
+router.get("/api/todos/:id", function(request, response) {
+  let id = request.params.id;
+  Todo.find({ _id: id }, function(err, docs) {
+    if (err) {
+      console.log(err);
+    } else {
+      response.json(docs);
+    }
+  });
+});
+
+//POST
+router.post("/api/todos", function(request, response) {
+  let title = request.body.title;
+  let content = request.body.content;
+  Todo.create({ title: title, content: content }, function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(request.body);
+      response.status(200).send(request.body);
+    }
+  });
+});
+
+//PUT
+router.put("/api/todos/:id", function(request, response) {
+  console.log(request.body.title);
+  console.log(request.body.content);
+  Todo.findById(request.params.id, function(err, todo) {
+    if (err) {
+      console.log(err);
+    }
+    todo.title = request.body.title;
+    todo.content = request.body.content;
+
+    //Save the todo
+    todo.save(function(err) {
+      if (err) {
+        console.log(err);
+      }
+      response.send("Sucess");
+    });
+  });
+});
+
+//DELETE
+router.delete("/api/todos/:id", function(request, response) {
+  let id = request.params.id;
+  Todo.deleteOne({ _id: id }, function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      response.sendStatus(200);
+    }
+  });
+});
+
+module.exports = router;
