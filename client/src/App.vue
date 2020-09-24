8da01bc1d7255da7fe0745ef74416d2610a1a02f
Adrian Palomares <adrianpal45@gmail.com>
2020-09-23 21:08:13 -0700

    Formatting

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,44 @@
 <template>
   <div id="app">
     <div class="container">
-      <input class="form-control" type="text" placeholder="Enter todo" v-model="addTitleInput" @keyup.enter="addTodo"/>
+      <h1>Todo App</h1>
+      <input
+        class="form-control"
+        type="text"
+        placeholder="Enter todo"
+        v-model="addTitleInput"
+        @keyup.enter="addTodo"
+      />
       <ul class="list-group">
-        <li class="list-group-item" v-for="t in arrayOfTodos" v-bind:todo-id="t._id" v-bind:key="t._id">
+        <li
+          class="list-group-item"
+          v-for="t in arrayOfTodos"
+          v-bind:todo-id="t._id"
+          v-bind:key="t._id"
+        >
           <span v-if="!t.isEditable">
-            <span @click="setEditable(t)">{{t.title}}</span>
-            <button class="btn btn-danger" style="float:right;" @click="deleteTodo(t)">Delete</button>
+            <span @click="setEditable(t)">{{ t.title }}</span>
+            <button
+              class="btn btn-danger"
+              style="float: right"
+              @click="deleteTodo(t)"
+            >
+              Delete
+            </button>
           </span>
           <span v-else>
-            <input type="text"  v-bind:value="t.title" @keyup.enter="editTodo($event, t)"/>
-            <button class="btn btn-success" style="float:right;" @click="editTodoButton($event, t)">Save</button>
+            <input
+              type="text"
+              v-bind:value="t.title"
+              @keyup.enter="editTodo($event, t)"
+            />
+            <button
+              class="btn btn-success"
+              style="float: right"
+              @click="editTodoButton($event, t)"
+            >
+              Save
+            </button>
           </span>
         </li>
       </ul>
@@ -19,110 +47,116 @@
 </template>
 
 <script>
-
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {},
-  data: function() {
+  data: function () {
     return {
       arrayOfTodos: [],
       addTitleInput: "",
       addContentInput: "",
-    }
+    };
   },
   methods: {
     /*
         For development purposes.
     */
-    printValue: function() {
+    printValue: function () {
       console.log(this.addTitleInput, this.addContentInput);
     },
-    addTodo: function() {
+    addTodo: function () {
       const title = this.addTitleInput;
       axios({
         method: "POST",
         url: "api/todos",
         headers: {
-          'Content-Type': "application/json"
+          "Content-Type": "application/json",
         },
         data: {
-          title: title
-        }
-      }).then(res =>  {
-        this.arrayOfTodos.push(res.data);
-      }).catch(err => console.log(err));
+          title: title,
+        },
+      })
+        .then((res) => {
+          this.arrayOfTodos.push(res.data);
+        })
+        .catch((err) => console.log(err));
       // Clear the input
       this.addTitleInput = "";
     },
-    deleteTodo: function(t) {
+    deleteTodo: function (t) {
       axios({
         method: "DELETE",
         url: `api/todos/${t._id}`,
         headers: {
-          'Content-Type': "application/json"
-        }
-      }).then(() => {
-        t.isRemoved = true;
-        this.arrayOfTodos = this.arrayOfTodos.filter(function(todo) {
-          return !todo.isRemoved;
+          "Content-Type": "application/json",
+        },
+      })
+        .then(() => {
+          t.isRemoved = true;
+          this.arrayOfTodos = this.arrayOfTodos.filter(function (todo) {
+            return !todo.isRemoved;
+          });
         })
-      }).catch(err => console.log(err));
-      
+        .catch((err) => console.log(err));
     },
-    setEditable: function(t) {
+    setEditable: function (t) {
       this.arrayOfTodos[this.arrayOfTodos.indexOf(t)].isEditable = true;
       this.$forceUpdate();
     },
     // Submitting edit through ENTER keypress
-    editTodo: function(event, t) {
+    editTodo: function (event, t) {
       // Grabbing the current title
       const todoTitle = event.currentTarget.value;
       axios({
         method: "PUT",
         url: `api/todos/${t._id}`,
         headers: {
-          'Content-Type': "application/json"
+          "Content-Type": "application/json",
         },
         data: {
-          title: todoTitle
-        }
-      }).then(() => {
-        t.title = todoTitle;
-        t.isEditable = false;
-        this.$forceUpdate();
-      }).catch(err => console.log(err));
+          title: todoTitle,
+        },
+      })
+        .then(() => {
+          t.title = todoTitle;
+          t.isEditable = false;
+          this.$forceUpdate();
+        })
+        .catch((err) => console.log(err));
     },
-    editTodoButton: function(event, t) {
+    editTodoButton: function (event, t) {
       const todoTitle = event.currentTarget.previousSibling.value;
       //Send info to api
       axios({
         method: "PUT",
         url: `api/todos/${t._id}`,
         headers: {
-          'Content-Type': "application/json"
+          "Content-Type": "application/json",
         },
         data: {
-          title: todoTitle
-        }
-      }).then(() => {
-        t.title = todoTitle;
-        t.isEditable = false;
-        this.$forceUpdate();
-      }).catch(err => {
-        console.log(err);
-      }) 
-    }
+          title: todoTitle,
+        },
+      })
+        .then(() => {
+          t.title = todoTitle;
+          t.isEditable = false;
+          this.$forceUpdate();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
   },
   /*
     This function will load the todos
   */
-  created: async function() {
-    const response = await axios('api/todos');
+  created: async function () {
+    const response = await axios("api/todos");
     this.arrayOfTodos = response.data;
-  }
-}
+  },
+};
 </script>
 
 <style>
@@ -132,5 +166,4 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-
 </style>
