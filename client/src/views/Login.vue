d568b81e92548fdddb4628597a43fe401a75283d
Adrian Palomares <adrianpal45@gmail.com>
2020-09-25 20:03:51 -0700

    Setup login page

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,7 +1,46 @@
 <template>
-  <h1>Log in Page</h1>
+    <div class="container">
+        <h1 class="mb-4">Log In</h1>
+        <form @submit="submitForm">
+            <div class="form-group">
+                <label for="inputUsername">Username</label>
+                <input
+                    type="text"
+                    class="form-control"
+                    id="inputUsername"
+                    placeholder="Username"
+                    v-model="username"
+                />
+            </div>
+            <div class="form-group">
+                <label for="inputPassword">Password</label>
+                <input
+                    type="password"
+                    class="form-control"
+                    id="inputPassword"
+                    placeholder="Password"
+                    v-model="password"
+                />
+            </div>
+            <button type="submit" class="btn btn-primary">Login</button>
+        </form>
+    </div>
 </template>
-
 <script>
-export default {};
+export default {
+    name: "Login",
+    data: function() {
+        return {
+            username: "",
+            password: "",
+        };
+    },
+    methods: {
+        submitForm: function(e) {
+            e.preventDefault();
+            console.log(this.username);
+            // TODO: AJAX request to api
+        },
+    },
+};
 </script>
