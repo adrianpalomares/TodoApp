01fc42da02696bac617d2138ebc5cdd8916f0696
Adrian Palomares <adrianpal45@gmail.com>
2020-09-27 12:41:41 -0700

    Clearing localStorage on log out

--- client/src/views/Logout.vue
+++ client/src/views/Logout.vue
@@ -1,15 +1,18 @@
 <template>
-    <h1>Log out</h1>
+  <h1>Log out</h1>
 </template>
 <script>
 import axios from "axios";
 export default {
-    name: "Logout",
-    created: function() {
-        axios({
-            method: "POST",
-            url: "/api/auth/logout",
-        }).then(() => this.$router.push("/login"));
-    },
+  name: "Logout",
+  created: function () {
+    axios({
+      method: "POST",
+      url: "/api/auth/logout",
+    }).then(() => {
+      localStorage.removeItem("user");
+      this.$router.push("/");
+    });
+  },
 };
 </script>
