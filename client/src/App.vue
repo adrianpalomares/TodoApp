a632d1acd0cc6b5e271ab9066f1ad66639708d07
Adrian Palomares <adrianpal45@gmail.com>
2020-09-26 21:57:04 -0700

    Changed link

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,9 @@
 <template>
     <div id="app">
+        <nav class="nav">
+            <router-link to="/" class="nav-link">Dashboard</router-link>
+            <router-link to="/logout" class="nav-link">Logout</router-link>
+        </nav>
         <router-view />
     </div>
 </template>
