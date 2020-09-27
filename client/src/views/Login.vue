649510f611a66caca20ff555f7f212125eebb0ff
Adrian Palomares <adrianpal45@gmail.com>
2020-09-27 10:54:07 -0700

    modified login view

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,46 +1,61 @@
 <template>
-    <div class="container">
-        <h1 class="mb-4">Log In</h1>
-        <form @submit="submitForm">
-            <div class="form-group">
-                <label for="inputUsername">Username</label>
-                <input
-                    type="text"
-                    class="form-control"
-                    id="inputUsername"
-                    placeholder="Username"
-                    v-model="username"
-                />
-            </div>
-            <div class="form-group">
-                <label for="inputPassword">Password</label>
-                <input
-                    type="password"
-                    class="form-control"
-                    id="inputPassword"
-                    placeholder="Password"
-                    v-model="password"
-                />
-            </div>
-            <button type="submit" class="btn btn-primary">Login</button>
-        </form>
-    </div>
+  <div class="container">
+    <h1 class="mb-4">Log In</h1>
+    <form @submit="submitForm">
+      <div class="form-group">
+        <label for="inputUsername">Username</label>
+        <input
+          type="text"
+          class="form-control"
+          id="inputUsername"
+          placeholder="Username"
+          v-model="username"
+        />
+      </div>
+      <div class="form-group">
+        <label for="inputPassword">Password</label>
+        <input
+          type="password"
+          class="form-control"
+          id="inputPassword"
+          placeholder="Password"
+          v-model="password"
+        />
+      </div>
+      <button type="submit" class="btn btn-primary">Login</button>
+    </form>
+  </div>
 </template>
 <script>
+import axios from "axios";
+// TODO: Add a redirect over to dashboard when user finishes loggging in
+// TODO: Have a check when view loads, (is user logged in?)
 export default {
-    name: "Login",
-    data: function() {
-        return {
-            username: "",
-            password: "",
-        };
-    },
-    methods: {
-        submitForm: function(e) {
-            e.preventDefault();
-            console.log(this.username);
-            // TODO: AJAX request to api
+  name: "Login",
+  data: function () {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    submitForm: function (e) {
+      e.preventDefault();
+      console.log(this.username);
+
+      axios({
+        url: "/api/auth/login",
+        method: "POST",
+        data: {
+          username: this.username,
+          password: this.password,
         },
+      }).then((res) => {
+        console.log(res);
+        localStorage.setItem("user", JSON.stringify(res.data.user));
+        this.$router.push("/");
+      });
     },
+  },
 };
 </script>
