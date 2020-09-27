55492758fa601e69ad21cd162c455d1bf99160e3
Adrian Palomares <adrianpal45@gmail.com>
2020-09-26 19:40:19 -0700

    Added logout view

---
+++ client/src/views/Logout.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>Log out</h1>
+</template>
+<script>
+import axios from "axios";
+export default {
+    name: "Logout",
+    created: function() {
+        axios({
+            method: "POST",
+            url: "/api/auth/logout",
+        }).then(() => console.log("Logged out"));
+    },
+};
+</script>
