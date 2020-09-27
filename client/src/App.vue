776209209f61f0d5e2deb7b3a84e49d918a0f3c8
Adrian Palomares <adrianpal45@gmail.com>
2020-09-27 10:56:31 -0700

    Changed link

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,15 +1,15 @@
 <template>
-    <div id="app">
-        <nav class="nav">
-            <router-link to="/" class="nav-link">Dashboard</router-link>
-            <router-link to="/logout" class="nav-link">Logout</router-link>
-        </nav>
-        <router-view />
-    </div>
+  <div id="app">
+    <nav class="nav">
+      <router-link to="/dashboard" class="nav-link">Dashboard</router-link>
+      <router-link to="/logout" class="nav-link">Logout</router-link>
+    </nav>
+    <router-view />
+  </div>
 </template>
 
 <script>
 export default {
-    name: "App",
+  name: "App",
 };
 </script>
