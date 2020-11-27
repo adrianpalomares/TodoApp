0b29ac67a24c6a76dafe875a4b47d5b11f174056
Adrian Palomares <adrianpal45@gmail.com>
2020-11-26 19:49:02 -0800

    fixed environment variable error

--- server/_config.js
+++ server/_config.js
@@ -3,7 +3,7 @@ const config = {};
 config.mongoURI = {
     development: "mongodb://localhost:27017/todoapp",
     test: "mongodb://localhost:27017/todoapp-test",
-    production: process.env.MONGO_URL,
+    production: process.env.MONGODB_URL,
 };
 
 module.exports = config;
